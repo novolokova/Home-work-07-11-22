f7ab931d8648b76bd3d262a4b86fdb3f12b38960
Margarita <m.novolokova@gmail.com>
2022-11-07 12:19:08 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let number1 = 45;
+let number2 = 12;
+let sum = number1+number2;
+console.log(sum);
+let deferent = number1-number2;
+console.log(deferent);
+let division = number1/number2;
+console.log(division);
+let multiplication = number1*number2;
+console.log(multiplication);
+
+
+let num = 456; 
+ num += 6; 
+ num++;
+ num /= 2;  
+ num -= 3; 
+ num--;
+ num %= 4; 
+ num += 'qwerty'; 
+
+ 
+
+
+
